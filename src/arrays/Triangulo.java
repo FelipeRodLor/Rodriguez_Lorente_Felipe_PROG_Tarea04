ef3b2114248465063ca8aa84d31f2dbed87cd4c0
FelipeRodriguezLorente <>
2017-11-28 20:43:35 +0100

    Se crea la clase Triangulo con sus tres atributos.

---
+++ src/arrays/Triangulo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arrays;
+
+/**
+ *
+ * @author Felipillo
+ */
+public class Triangulo {
+   
+    private Punto puntoA;
+    private Punto puntoB;
+    private Punto puntoC;
+    
+}
