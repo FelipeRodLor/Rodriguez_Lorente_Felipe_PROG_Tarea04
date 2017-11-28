85bc2a83d65c0a289c80317cc726f2ab74e650b5
FelipeRodriguezLorente <>
2017-11-28 20:36:03 +0100

    Se renombra el paquete Tarea04 creado por defecto al crear el proyecto por arrays con la clase Punto y sus dos atributos.

--- src/arrays/Punto.java
+++ src/arrays/Punto.java
@@ -3,19 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tarea04;
+package arrays;
 
 /**
  *
  * @author Felipillo
  */
-public class Tarea04 {
+public class Punto {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+    private double x;
+    private double y;
+
 }
